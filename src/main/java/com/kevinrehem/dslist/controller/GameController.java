6727372fe1f3180a89df7b339400a03fe1ae6ec9
Kevin Rehem <kevin.rehem@gmail.com>
2025-05-20 18:26:40 -0300

    GameController

--- src/main/java/com/kevinrehem/dslist/controller/GameController.java
+++ src/main/java/com/kevinrehem/dslist/controller/GameController.java
@@ -1,4 +1,26 @@
 package com.kevinrehem.dslist.controller;
 
+import com.kevinrehem.dslist.dto.GameMinDTO;
+import com.kevinrehem.dslist.entities.Game;
+import com.kevinrehem.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value ="/games")
 public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll(){
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+
 }
