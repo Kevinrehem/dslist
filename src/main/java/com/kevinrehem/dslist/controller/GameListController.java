7c750703bfa69c37383428685c0c268135b9b97c
Kevin Rehem <kevin.rehem@gmail.com>
2025-05-21 00:59:32 -0300

    GameListController FindByList

--- src/main/java/com/kevinrehem/dslist/controller/GameListController.java
+++ src/main/java/com/kevinrehem/dslist/controller/GameListController.java
@@ -1,7 +1,9 @@
 package com.kevinrehem.dslist.controller;
 
 import com.kevinrehem.dslist.dto.GameListDTO;
+import com.kevinrehem.dslist.dto.GameMinDTO;
 import com.kevinrehem.dslist.services.GameListService;
+import com.kevinrehem.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,9 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping(value = "/{id}")
     public GameListDTO findByID(@PathVariable Long id){
         return gameListService.findByID(id);
@@ -25,6 +30,11 @@ public class GameListController {
     @GetMapping
     public List<GameListDTO> findAll(){
         return gameListService.findAll();
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId){
+        return gameService.findByList(listId);
     }
 
 }
